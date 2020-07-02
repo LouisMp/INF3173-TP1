aa05b5665c6cec92691215af55527f9cb26cf920
LouisMp <louismp77@gmail.com>
2020-07-02 09:31:25 -0500

    error fix

--- monshell.c
+++ monshell.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "tools.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char **envp)
 { 
-    return EXIT_SUCCES;
+    environnement = envp;
+    if(argc==2) {
+        FILE *fichier = NULL;
+        fichier = fopen(argv[1], "r");
+
+        if (fichier == NULL){
+            fprintf(stderr, "Usage: %s <batchfile> can't open file\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+        monshell_batchfile(fichier);
+
+    } else if(argc==1) {
+        monshell_cmdline();
+    } else {
+        fprintf(stderr, "Usage: %s <batchfile> or no arguments\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    return EXIT_SUCCESS;
 }
